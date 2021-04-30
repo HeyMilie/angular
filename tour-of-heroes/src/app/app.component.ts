d9991c4f4687e7123f6af1d737b052796047fe78
heymilie <emilie.delaurent@gmail.com>
2021-04-30 14:21:44 +0200

    prise en main d'angular avec le tuto

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+let heroCount =2;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'La tour des héros';
+  titleStyle = {'font-family' : 'Arial', 'color' : 'red'};
+  username = "Anonymous";
+  heros = [
+    "Batman",
+    "Catwoman",
+    "Wonder Woman",
+    "Iron Man"
+  ];
+  heroToAdd = '';
+  addHero(){
+    this.heros.push(this.heroToAdd);
+    this.title = 'La tour des ' + this.heros.length + ' héros';
+  }
 }
